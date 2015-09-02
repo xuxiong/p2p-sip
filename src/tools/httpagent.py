084fda9503132eea6b1330b6435a39eeb6615375
xuxiong <xuxiong@gsta.com>
2015-09-02 16:54:24 +0800

    .

---
+++ src/tools/httpagent.py
@@ -0,0 +1,19 @@
+import BaseHTTPServer
+import subprocess
+
+cmds = ['netstat']
+
+class ShellRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+  def do_GET(self):
+    cmd, args = self.path.split('/')
+    if cmd in cmds:	
+      s = subprocess.call([cmd], stdout=self.wfile)
+    else:
+      self.send_error(405)	
+
+def test(HandlerClass = ShellRequestHandler,
+         ServerClass = BaseHTTPServer.HTTPServer):
+    BaseHTTPServer.test(HandlerClass, ServerClass)
+
+if __name__ == '__main__':
+    test()	
