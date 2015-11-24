f6c13a05ffaa3533420387099044b868e0242583
xuxiong <xuxiong@gsta.com>
2015-11-24 09:09:44 +0800

    command line

--- src/answermachine.py
+++ src/answermachine.py
@@ -1,12 +1,18 @@
-import sys, logging
+import sys, logging, gevent
 
 try: 
   from app import voip, sipstackcaller
   from std import rfc3261, rfc2396, rfc3550, rfc4566, kutil
-  from external import log
+  #from external import log
 except ImportError: print 'Please install p2p-sip and include p2p-sip/src and p2p-sip/src/external in your PYTHONPATH'; traceback.print_exc(); sys.exit(1)
 
 logger = logging.getLogger('answermachine')
+
+try:
+    from subprocess import DEVNULL # py3k
+except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
 
 class Options(object):
   def __init__(self, user, domain, password, bac=None, int_ip='0.0.0.0', port=0, register=True, register_interval=3600, retry_interval=60, auto_respond=200, auto_respond_after=3, auto_terminate_after=0, transports='udp', listen_queue=5, max_size=4096, fix_nat=False, user_agent=None, strict_route=False, uri=None, proxy='', send='', listen=True):
@@ -48,21 +54,33 @@ class Call(sipstackcaller.Call):
     video.fmt.append( rfc4566.attrs(pt=102, name='H264', rate=90000) )
     video.fmt.append( rfc4566.attrs(pt=103, name='VP8', rate=90000) )
     video.fmt.append( rfc4566.attrs(pt=104, name='H264', rate=90000,fmt_params="packetization-mode=1"))
+    video.direction = 'sendonly'
     self._audio_and_video_streams, self._queue = [audio, video], []
         
 if __name__ == '__main__': 
+  def addjob(jobs, answerers, username, password, bac, int_ip):
+    (user, domain) = username.split('@')
+    options = Options(user, domain, password, bac=bac, int_ip=int_ip)
+    answerer = Answerer(options)
+    jobs.append(gevent.spawn(answerer.wait))
+    answerers.append(answerer)
+
   try:
     handler = logging.StreamHandler(stream=sys.stdout)
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(logging.Formatter('%(asctime)s|%(module)s|%(lineno)s|%(levelname)s|%(message)s', datefmt='%H:%M:%S'))
     logging.getLogger().addHandler(handler)
-
+    if hasattr(rfc3261, 'log'): 
+      rfc3261.log.setLevel(logging.DEBUG)
+      rfc3261.log.addHandler(handler)
     argv = sys.argv
     i, username, password, media, bac = 1, None, None, None, None
     int_ip = '0.0.0.0' 
+    jobs, answerers = [], []
     while i < len(argv):
       if argv[i] == '-u':
         if username:		
+          addjob(jobs, answerers, username, password, bac, int_ip)
           username, password, media = None, None, None		
         username = argv[i+1]
       elif argv[i] == '-p':
@@ -75,18 +93,18 @@ if __name__ == '__main__':
         int_ip = argv[i+1]
       i += 2		
 
-    (user, domain) = username.split('@')
-    options = Options(user, domain, password, bac=bac, int_ip=int_ip)
-
-    answerer = Answerer(options)
-    answerer.wait()
+    addjob(jobs, answerers, username, password, bac, int_ip)
+    gevent.joinall(jobs)
+  
   except KeyboardInterrupt:
     print '' # to print a new line after ^C
   except: 
     logger.exception('exception')
     sys.exit(-1)
+  
   try:
-    answerer.close()
+    jobs = [gevent.spawn(x.close) for x in answerers]
+    gevent.joinall(jobs, timeout=2)
   except KeyboardInterrupt:
     print ''
-
+  
